9bfec94002c6df20041d655f400b921391e43209
ahmed <ahmedeissa2016@gmail.com>
2021-12-07 13:18:33 +0200

    'finally'

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const { argv } = require('yargs')
 const yargs = require('yargs')
+const student =require('./students.js')
 
 
 
@@ -6,18 +8,60 @@ yargs.command({
     command:'add',
     describe:'',
     builder:{
+        id:{
+            describe:'',
+            demandOption:true,
+            type:'string'
+        },
         name:{
             describe:'',
-            type:'array',
+            demandOption:true,
+            type:'string'
+        },
+        grades:{
+            describe:'',
             demandOption:true,
         }
     }
     ,
     handler:(argv)=>{
+        const grades =argv.grades.split(',')
+        const total =grades.reduce((all,elem)=> parseFloat(all) +parseFloat(elem) )
+        const name =argv.name
+        const id =argv.id
 
-        console.log( argv.name)
+       student.addStudent(id,name,grades,total)
+
     }
 })
 
+yargs.command({
+    command:'delete',
+    describe:'',
+    builder:{
+       id:{
+        describe:'',
+        demandOption:true,
+       }
+    },
+    handler:(argv)=>{
+        student.deleteStudent(argv.id)
+    }
+})
+yargs.command({
+    command:'list',
+    describe:'',
 
+    handler:(argv)=>{
+        student.listStudents()
+    }
+})
+yargs.command({
+    command:'*',
+    describe:'',
+
+    handler:(argv)=>{
+        console.log('not a command')
+    }
+})
 yargs.parse()
